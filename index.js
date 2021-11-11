61044ea9e7b0541f01524b887d8f349e42292c8a
pietow <pprotassow@posteo.de>
2021-11-11 11:17:10 +0100

    refacture delete post

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const title = document.querySelector('#title')
 const post = document.querySelector('#post')
 const btn = document.querySelector('.btn')
 const card = document.querySelector('.card')
-const cardTitle = document.querySelector('.card-header')
-const cardbody = document.querySelector('.card-body p')
+const footer = document.querySelector('footer')
+// const cardTitle = document.querySelector('.card-header')
+// const cardbody = document.querySelector('.card-body p')
 
 function handlePost() {
     fetch('https://jsonplaceholder.typicode.com/posts', {
@@ -19,12 +20,43 @@ function handlePost() {
     })
         .then(response => response.json())
         .then(json => {
-            card.classList.remove('d-none')
-            cardTitle.innerText = json.title
-            cardbody.innerText = json.body
-            title.value = ''
-            post.value = ''
+            if (!localStorage.posts) {
+                localStorage.posts = JSON.stringify([json])
+                renderPosts([json])
+            } else {
+                let appendJson = JSON.parse(localStorage.posts)
+                appendJson.push(json)
+                localStorage.posts = JSON.stringify(appendJson)
+                renderPosts(appendJson)
+            }
         })
 }
 
+
+
+const posts = localStorage.posts ? JSON.parse(localStorage.posts) : []
+renderPosts(posts)
 btn.addEventListener('click', handlePost)
+
+function renderPosts(data) {
+    deletePosts(footer)
+    // console.log(footer.children.length)
+    data.map((body) => {
+        const copyCard = card.cloneNode(true)
+        copyCard.classList.remove('d-none')
+        const cardTitle = copyCard.children[0]
+        const cardbody = copyCard.children[1].children[0]
+        cardTitle.innerText = body.title
+        cardbody.innerText = body.body
+        title.value = ''
+        post.value = ''
+        footer.append(copyCard)
+    })
+}
+
+function deletePosts(parent) {
+    if(parent.firstElementChild) {
+        parent.removeChild(parent.firstElementChild)
+        deletePosts(parent)
+    }
+}
